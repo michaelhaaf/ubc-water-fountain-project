9de65ea140df7d532f92c1ae01fd0ac83ec8ca5c
Michael Haaf <mich@elhaaf.com>
2015-02-07 13:40:21 -0800

    initial commit message

---
+++ src/com/WaterFountainApp/WaterFountainApp.java
@@ -0,0 +1,15 @@
+package com.WaterFountainApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WaterFountainApp extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
